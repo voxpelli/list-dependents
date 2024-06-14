7e5a813f0d630aabece9571cc4cd2331f0ace90d
Pelle Wessman <pelle@kodfabrik.se>
2024-06-14 23:03:08 +0200

    Update linting dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
+// eslint-disable-next-line n/no-missing-import
 export * from './lib/main.js';
-export type * from './lib/interface-types.js';
+export type * from './lib/interface-types.d.ts';
--- lib/interface-types.d.ts
+++ lib/interface-types.d.ts
@@ -1,12 +1,14 @@
 import type { BunyanLite } from "bunyan-adaptor";
 import { Got } from "got";
 // @ts-ignore
+// eslint-disable-next-line n/no-unpublished-import
 import type { NormalizedPackageJson } from "read-pkg";
 
 declare global {
   // Trying to upstream here: https://github.com/octet-stream/form-data-encoder/pull/29
-  // The <R = any> is needed for compatibility with the ReadableStream from lib.dom.d.ts
-  interface ReadableStream<R = any> extends NodeJS.ReadableStream {}
+  // The <_R = any> is needed for compatibility with the ReadableStream from lib.dom.d.ts
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface ReadableStream<_R = any> extends NodeJS.ReadableStream {}
 }
 
 export interface DependentsMeta {
